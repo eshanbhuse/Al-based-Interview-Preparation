3d9120db118184c676bb21b4e3b66c0312733ba3
Eshan Bhuse <eshan.bhuse@spit.ac.in>
2025-09-24 23:01:37 +0530

    updated navbar

--- frontend/interview-prep-ai/src/components/layouts/Navbar.jsx
+++ frontend/interview-prep-ai/src/components/layouts/Navbar.jsx
@@ -5,8 +5,8 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <div className='h-16 bg-white border-b border-gray-200/50 backdrop-blur-[2px] sticky top-0 z-30'>
-            <div className='container mx-auto flex items-center justify-between px-6 md:px-12 h-full'>
+        <div className='h-16 bg-white border-b border-gray-200/50 backdrop-blur-[2px] py-2.5 px-4 md:px-0 sticky top-0 z-30'>
+            <div className='container mx-auto flex items-center justify-between gap-5'>
                 {/* Left side */}
                 <Link to="/dashboard" className='flex items-center h-full'>
                     <h2 className='text-lg md:text-xl font-medium text-black leading-5'>
